6d23ad964de49bfc21d6ceb4a2b855fb6730c746
mennahub <mennaalaa111222@gmail.com>
2023-04-30 04:00:43 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const availableCities = new Set(['Saint Catherine', 'Nuweiba', 'Ras Sheetan', 'Taba', 'Dahab']);
+const availableCities = new Set(['Saint Catherine', 'Nuweiba', 'Ras Shetaan', 'Taba', 'Dahab']);
 const packagesMap = new Map([
     ['Dahab 1', {
         campName: 'Zouqaq',
@@ -6,12 +6,12 @@ const packagesMap = new Map([
         durationDays: 5,
         persons: 2,
         price: 4200,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -Visit the 3 pools.
-            -Safari.
+        description: `<br>
+            -Room with 2 beds & bathroom.<br>
+            -Trip for Lagonna.<br>
+            -Visit the 3 pools.<br>
+            -Safari.<br>
+            -Transportation.
         `
     }],
     ['Dahab 2', {
@@ -20,12 +20,12 @@ const packagesMap = new Map([
         durationDays: 5,
         persons: 2,
         price: 4500,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 1 beds & bathroom.
-            -Visit the Blue Lagon.
-            -Visit the Blue Hole.
-            -Visit AbuGaloum.
+        description: `<br>
+        -Room with 1 beds & bathroom.<br>
+            -Visit the Blue Lagon.<br>
+            -Visit the Blue Hole.<br>
+            -Visit AbuGaloum.<br>
+            -Transportation.
         `
     }],
     ['Dahab 3', {
@@ -34,180 +34,172 @@ const packagesMap = new Map([
         durationDays: 5,
         persons: 2,
         price: 4700,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -A Trip Full Board in the sea at yachit.
-            -Safari
+        description: `<br>
+            -Room with 2 beds & bathroom.<br>
+            -Trip for Lagonna.<br>
+            -A Trip Full Board in the sea at yachit.<br>
+            -Safari.<br>
+            -Transportation.
         `
     }],
     ['Nuweiba 1', {
-        campName: 'Zouqaq',
+        campName: 'Magarra',
         place: 'Nuweiba',
         durationDays: 5,
         persons: 2,
-        price: 4200,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -Visit the 3 pools.
-            -Safari.
+        price: 2650,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Trip for Wadi el Washwashi.<br>
+        -Visit the Coloured Canyon.<br>
+        -Snorkeling.<br>
+        -Transportation.
         `
     }],
     ['Nuweiba 2', {
-        campName: 'Zouqaq',
+        campName: 'Magarra',
         place: 'Nuweiba',
         durationDays: 5,
         persons: 2,
-        price: 4500,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 1 beds & bathroom.
-            -Visit the Blue Lagon.
-            -Visit the Blue Hole.
-            -Visit AbuGaloum.
+        price: 2800,
+        description: `<br>
+        -Room with 1 beds & bathroom.<br>
+        -Visit Abu Hamada Canyon.<br>
+        -Visit Nawamis Area.<br>
+        -Safari.<br>
+        -Transportation.
         `
     }],
     ['Nuweiba 3', {
-        campName: 'Zouqaq',
+        campName: 'Magarra',
         place: 'Nuweiba',
         durationDays: 5,
         persons: 2,
-        price: 4700,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -A Trip Full Board in the sea at yachit.
-            -Safari
+        price: 2950,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Snorkeling.<br>
+        -Visit the Nuweiba Castle.<br>
+        -Visit Al Tarabin Castle.<br>
+        -Transportation.
         `
     }],
     ['Taba 1', {
-        campName: 'Zouqaq',
+        campName: 'Run Away',
         place: 'Taba',
         durationDays: 5,
         persons: 2,
-        price: 4200,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -Visit the 3 pools.
-            -Safari.
+        price: 2300,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Scuba Diving.<br>
+        -Visit Coloured Cayon.<br>
+        -Castle Zaman.<br>
+        -Transportaion.
         `
     }],
     ['Taba 2', {
-        campName: 'Zouqaq',
+        campName: 'Run Away',
         place: 'Taba',
         durationDays: 5,
         persons: 2,
-        price: 4500,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 1 beds & bathroom.
-            -Visit the Blue Lagon.
-            -Visit the Blue Hole.
-            -Visit AbuGaloum.
+        price: 2500,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Visit Wishwashi Canyon.<br>
+        -Visit Pharaoh's island.<br>
+        -Visit Ras Mohammed Park.<br>
+        -Transportaion.
         `
     }],
     ['Taba 3', {
-        campName: 'Zouqaq',
+        campName: 'Run Away',
         place: 'Taba',
         durationDays: 5,
         persons: 2,
-        price: 4700,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -A Trip Full Board in the sea at yachit.
-            -Safari
+        price: 2950,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Full day at Fajod Bay.<br>
+        -Scuba Diving.<br>
+        -Visit Ras Mohammed Park.<br>
+        -Transportaion.
         `
     }],
-    ['Ras Sheetan 1', {
-        campName: 'Zouqaq',
+    ['Ras Shetaan 1', {
+        campName: 'Mixer',
+        place: 'Ras Shetaan',
+        durationDays: 5,
+        persons: 2,
+        price: 2250,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Diving in Ras Mamlish.<br>
+        -Visit the Coloured Canyon.<br>
+        -Tranportations.
+        `
+    }],
+    ['Ras Shetaan 2', {
+        campName: 'Mixer',
         place: 'Ras Sheetan',
         durationDays: 5,
         persons: 2,
-        price: 4200,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -Visit the 3 pools.
-            -Safari.
+        price: 2350,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Diving in Dense.<br>
+        -Trip for Sayedeen Village.<br>
+        -Tranportations.
         `
     }],
-    ['Ras Sheetan 2', {
-        campName: 'Zouqaq',
+    ['Ras Shetaan 3', {
+        campName: 'Mixer',
         place: 'Ras Sheetan',
         durationDays: 5,
         persons: 2,
-        price: 4500,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 1 beds & bathroom.
-            -Visit the Blue Lagon.
-            -Visit the Blue Hole.
-            -Visit AbuGaloum.
-        `
-    }],
-    ['Ras Sheetan 3', {
-        campName: 'Zouqaq',
-        place: 'Ras Sheetan',
-        durationDays: 5,
-        persons: 2,
-        price: 4700,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -A Trip Full Board in the sea at yachit.
-            -Safari
+        price: 2500,
+        description: `<br>
+        -Room with 2 beds & bathroom.<br>
+        -Trip for Sayedeen Village.<br>
+        -Visit the Nuwamis.<br>
+        -Tranportations.
         `
     }],
     ['Saint Catherine 1', {
-        campName: 'Zouqaq',
+        campName: 'Gebal',
         place: 'Saint Catherine',
-        durationDays: 5,
-        persons: 2,
-        price: 4200,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -Visit the 3 pools.
-            -Safari.
+        durationDays: 3,
+        persons: 1,
+        price: 400,
+        description: `<br>
+        -Trip for 3 Pools.<br>
+        -Hiking to Gabal Mousa.<br>
+        -Transportation.
         `
     }],
     ['Saint Catherine 2', {
-        campName: 'Zouqaq',
+        campName: 'Gebal',
         place: 'Saint Catherine',
-        durationDays: 5,
-        persons: 2,
-        price: 4500,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 1 beds & bathroom.
-            -Visit the Blue Lagon.
-            -Visit the Blue Hole.
-            -Visit AbuGaloum.
+        durationDays: 3,
+        persons: 1,
+        price: 500,
+        description: `<br>
+        -Trip for 3 Pools & Safari.<br>
+        -Hiking to Gabal Mousa.<br>
+        -Transportation.
         `
     }],
     ['Saint Catherine 3', {
-        campName: 'Zouqaq',
+        campName: 'Gebal',
         place: 'Saint Catherine',
-        durationDays: 5,
-        persons: 2,
-        price: 4700,
-        description: `
-            You will enjoy your holiday in Zouqaq Camp in a simple way; This package contain:
-            -Room with 2 beds & bathroom.
-            -Trip for Lagonna.
-            -A Trip Full Board in the sea at yachit.
-            -Safari
+        durationDays: 3,
+        persons: 1,
+        price: 700,
+        description: `<br>
+        -Trip for 3 Pools & Safari.<br>
+        -Hiking to Gabal Mousa.<br>
+        -A night at Gabl Al Tawelaat.<br>
+        -Transportation.
         `
     }],
 ]);
@@ -304,6 +296,8 @@ const packagesMap = new Map([
             }
         }
     });
+
+    //login,signup
     
 })(jQuery);
 var modal = document.getElementById('id01');
